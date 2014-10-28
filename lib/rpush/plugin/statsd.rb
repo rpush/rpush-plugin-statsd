54c89bbb7b59db6ea918547c0a07d720ce5a4eff
Ian Leitch <port001@gmail.com>
2014-10-27 17:42:06 -0700

    Avoid using Rpush namespaces.

--- lib/rpush/plugin/statsd.rb
+++ lib/rpush/plugin/statsd.rb
@@ -1,25 +1,19 @@
 require 'rpush/plugin/statsd/version'
 
-module Rpush
-  module Plugin
-    module Statsd
-      plugin = Rpush.plugin(:statsd)
-      plugin.url = 'https://github.com/rpush/rpush-plugin-statsd'
-      plugin.description = 'Rpush plugin to sends stats to StatsD.'
+plugin = Rpush.plugin(:statsd)
+plugin.url = 'https://github.com/rpush/rpush-plugin-statsd'
+plugin.description = 'Rpush plugin to sends stats to StatsD.'
 
-      plugin.init do
-        @statsd = ::Statsd.new(plugin.config.host, plugin.config.port)
-        @statsd.namespace = 'rpush'
-      end
+plugin.configure do |config|
+  config.host = 'localhost'
+  config.port = 9125
+end
 
-      plugin.configure do |config|
-        config.host = 'localhost'
-        config.port = 9125
-      end
+plugin.reflect do |on|
+  on.error(_) { @statsd.increment(:error) }
+end
 
-      plugin.reflect do |on|
-        on.error(_) { @statsd.increment(:error) }
-      end
-    end
-  end
+plugin.init do
+  @statsd = ::Statsd.new(plugin.config.host, plugin.config.port)
+  @statsd.namespace = 'rpush'
 end
--- lib/rpush/plugin/statsd/version.rb
+++
@@ -1,7 +0,0 @@
-module Rpush
-  module Plugin
-    module Statsd
-      VERSION = "0.0.1"
-    end
-  end
-end
